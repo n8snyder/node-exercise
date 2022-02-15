21bfefea66b4469e21f2c8151ae1c3716d03a352
Nate Snyder <n8.snyder@berkeley.edu>
2022-02-15 15:13:02 -0800

    Refactored to have controler function

--- step3.js
+++ step3.js
@@ -5,65 +5,68 @@ const axios = require("axios");
 let inPath;
 let outFile;
 
+
 /** Reads a file and print to the console the contents. */
 
-async function cat(path, outFile="") {
+async function cat(path, outFile = "") {
     let contents;
     try {
         contents = await fsP.readFile(path, "utf8");
-        // console.log(contents);
     } catch (err) {
         console.log(err.message);
         process.exit(1);
     }
-    handleOutput(contents, outFile);
-    // if(outFile){
-    //     fsP.writeFile(outFile, contents, "utf8");
-    // } else {
-    //     console.log(contents);
-    // }
+    return contents;
 }
 
 
 /** Gets website HTML and prints it to the console. */
 
-async function webCat(url, outFile="") {
+async function webCat(url, outFile = "") {
     let resp;
     try {
         resp = await axios.get(url);
-        // console.log(resp.data);
     } catch (err) {
         console.log(`Error fetching ${url}`);
         console.log(err.message);
         process.exit(1);
     }
-    handleOutput(resp.data, outFile);
-    // if(outFile){
-    //     fsP.writeFile(outFile, resp.data, "utf8");
-    // } else {
-    //     console.log(resp.data);
-    // }
+    return resp.data;
 }
 
-function handleOutput(content, outFile=""){
 
-    if(outFile){
-        fsP.writeFile(outFile, content, "utf8");
+/** Handles how the content should be outputted, to a file or console. */
+
+function handleOutput(content, outFile = "") {
+    if (outFile) {
+        try {
+            fsP.writeFile(outFile, content, "utf8");
+        } catch (err) {
+            console.log(err.message);
+            process.exit(1);
+        }
     } else {
         console.log(content);
     }
-} 
+}
 
-if(process.argv[2] === "--out"){
+
+async function start(inPath, outFile = "") {
+    let content;
+    if (inPath.startsWith("http://") || inPath.startsWith("https://")) {
+        content = await webCat(inPath, outFile);
+    } else {
+        content = await cat(inPath, outFile);
+    }
+    handleOutput(content, outFile);
+}
+
+
+if (process.argv[2] === "--out") {
     inPath = process.argv[4];
     outFile = process.argv[3];
 } else {
     inPath = process.argv[2];
 }
 
-
-if (inPath.startsWith("http://") || inPath.startsWith("https://")) {
-    webCat(inPath, outFile);
-} else {
-    cat(inPath, outFile);
-}
+start(inPath, outFile);
